6b468d30d5d35d15d46cc9f2b7054a1f6d9ccd3e
jaimesilvalorca <jaimesilvalorca@gmail.com>
2023-10-12 13:28:49 -0300

    ft: usercontroller con register y getUsers

---
+++ src/controller/userController.js
@@ -0,0 +1,36 @@
+import userModel from "../models/usersModel.js";
+
+export const Register = async (req, res) => {
+    try {
+        const user = req.body;
+        const userAdded = await userModel.create(user);
+        res.status(201).json({
+            success: true,
+            message: 'El registro se realizó correctamente',
+            data: userAdded
+        });
+    } catch (err) {
+        res.status(500).json({
+            success: false,
+            message: 'Hubo un error con el registro del usuario',
+            error: err
+        });
+    }
+}
+
+
+export const getUsers = async (req,res)=>{
+    try {
+        const users = await userModel.find().lean().exec();
+
+        return res.json(users)
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: 'Hubo un error al obtener los usuarios',
+            error: err
+        });
+       
+    }
+
+}
