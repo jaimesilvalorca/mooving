fece8920d2f80f48a0064469006dc14385701aa3
jaimesilvalorca <jaimesilvalorca@gmail.com>
2023-11-08 01:52:44 -0300

    Ft: Cars Model

---
+++ src/models/carsModel.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+const carCollection = "cars";
+
+const carSchema = new mongoose.Schema({
+    make: {
+        type: String,
+        default: null
+    },
+    model: {
+        type: String,
+        default: null
+    },
+    year: {
+        type: Number,
+        default: null
+    },
+    plate: {
+        type: String,
+        default: null
+    },
+
+    created_at: {
+        type: Date,
+        default: Date.now,
+    },
+    updated_at: {
+        type: Date,
+        default: Date.now,
+    }
+});
+
+mongoose.set("strictQuery", false);
+const CarModel = mongoose.model(carCollection, carSchema);
+
+export default CarModel;
