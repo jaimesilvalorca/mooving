5bd47e78e242722af0619bc865bad1887618d19a
jaimesilvalorca <jaimesilvalorca@gmail.com>
2023-11-08 01:52:52 -0300

    Driver Models

---
+++ src/models/driverModel.js
@@ -0,0 +1,40 @@
+import mongoose, { Schema } from "mongoose";
+
+const driverCollection = "drivers";
+
+const driverSchema = new mongoose.Schema({
+    email: String,
+    name: String,
+    lastname: String,
+    phone: String,
+    car:{
+        type:Schema.Types.ObjectId,
+        ref: 'cars'
+    },
+    image: {
+        type: String,
+        default: null
+    },
+    password: String,
+    role:{
+        type:String,
+        default: 'user'
+    },
+    status:{
+        type:Boolean,
+        default:false
+    },
+    created_at: {
+        type: Date,
+        default: Date.now,
+    },
+    updated_at: {
+        type: Date,
+        default: Date.now,
+    }
+});
+
+mongoose.set("strictQuery", false);
+const DriverModel = mongoose.model(driverCollection, driverSchema);
+
+export default DriverModel;
